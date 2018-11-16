e1e95af0aa7bc24fbe7f0e9db14a1a046f2a3659
Guilherme Chichanoski <guilherme.chichanoski@gmail.com>
2018-11-15 23:42:09 -0200

    first try with codespan

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 extern crate clap;
+extern crate codespan;
+extern crate codespan_reporting;
 extern crate lalrpop_util;
 extern crate llvm_sys as llvm;
 
@@ -9,14 +11,14 @@ mod grammar;
 mod symbol_table;
 
 use clap::{app_from_crate, crate_authors, crate_description, crate_name, crate_version, Arg};
-use std::env;
-use std::fs;
+use codespan::{ByteIndex, CodeMap, Span};
+use codespan_reporting::termcolor::{ColorChoice, StandardStream};
+use codespan_reporting::{emit, Diagnostic, Label};
+use lalrpop_util::ParseError;
 use std::process::exit;
 
 enum ExitCodes {
-    Success = 0,
     FileError = 1,
-    BadFormatParam = 2,
 }
 
 fn main() {
@@ -30,24 +32,82 @@ fn main() {
         )
         .get_matches();
 
-    let input_file_name = match matches.value_of("input") {
-        Some(f) => f,
-        None => exit(ExitCodes::FileError as i32),
-    };
+    let input_file_name = matches.value_of("input").unwrap();
 
-    let content = match fs::read_to_string(input_file_name) {
-        Ok(c) => c,
-        Err(e) => {
-            println!("Error on file read: {}", e);
-            exit(ExitCodes::FileError as i32);
+    let mut code_map = CodeMap::new();
+    let writer = StandardStream::stderr(ColorChoice::Always);
+
+    let file = match code_map.add_filemap_from_disk(input_file_name) {
+        Ok(file) => file,
+        Err(_) => {
+            let file_error_diagnose =
+                Diagnostic::new_error(format!("no such file: {}", input_file_name));
+            emit(writer, &code_map, &file_error_diagnose).unwrap();
+            exit(ExitCodes::FileError as i32)
         }
     };
+
+    let content = file.src();
 
     unsafe {
         let mut erros = vec![];
         match grammar::ProgramParser::new().parse(&mut erros, &content) {
             Ok(expr) => gen::gen(expr),
-            Err(err) => println!("{:?}", err),
+            Err(err) => match err {
+                ParseError::UnrecognizedToken { token, expected } => {
+                    let (start, token, end) = match token {
+                        Some((start, token, end)) => (
+                            ByteIndex::from((start + 1) as u32),
+                            token.1,
+                            ByteIndex::from((end + 1) as u32),
+                        ),
+                        None => (ByteIndex::none(), "EOF", ByteIndex::none()),
+                    };
+
+                    let diagnostic = Diagnostic::new_error(format!(
+                        "{} found, but expect {:?}",
+                        token, expected
+                    ));
+
+                    let span = Span::new(start, end);
+
+                    let label = Label::new_primary(span);
+                    emit(writer, &code_map, &diagnostic.with_label(label)).unwrap();
+                }
+                ParseError::User { error } => {
+                    let diagnostic = Diagnostic::new_error(error);
+
+                    emit(writer, &code_map, &diagnostic).unwrap();
+                }
+                ParseError::ExtraToken { token } => {
+                    let (start, token, end) = (
+                        ByteIndex::from((token.0 + 1) as u32),
+                        token.1,
+                        ByteIndex::from((token.2 + 1) as u32),
+                    );
+
+                    let diagnostic =
+                        Diagnostic::new_error(format!("{} found, but was not expected", token));
+
+                    let span = Span::new(start, end);
+
+                    let label = Label::new_primary(span);
+                    emit(writer, &code_map, &diagnostic.with_label(label)).unwrap();
+                }
+                ParseError::InvalidToken { location } => {
+                    let span = file
+                        .line_span(
+                            file.find_line(ByteIndex::from((location + 1) as u32))
+                                .unwrap(),
+                        )
+                        .unwrap();
+
+                    let diagnostic = Diagnostic::new_error("invalid line or EOF");
+                    let label = Label::new_primary(span);
+
+                    emit(writer, &code_map, &diagnostic.with_label(label)).unwrap();
+                }
+            },
         }
     }
 }
